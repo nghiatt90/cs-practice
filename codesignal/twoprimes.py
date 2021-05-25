82425170f7086b2fac9edf7ee4a87990937c731d
Yoshi Truong <nghia.truong@tokyotechies.com>
2021-05-25 23:20:02 +0900

    twoprimes

---
+++ codesignal/twoprimes.py
@@ -0,0 +1,11 @@
+# https://app.codesignal.com/challenge/CRhgt6WA9dESv3NrW
+    int t, i;
+	boolean TwoPrimes(int a, int b) {
+	  return b(a) & b(b);
+	}
+	
+	boolean b(int a) {
+		for (t = i = 1; ++i < a;)
+			t *= a % i;
+		return t > 0 & a > 1;
+	}
