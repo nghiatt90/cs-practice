3d863a20fdaddcc9be95239ede8d6ac3358e771f
Yoshi Truong <nghia.truong@tokyotechies.com>
2021-05-25 23:19:00 +0900

    supercup

---
+++ codesignal/supercup.py
@@ -0,0 +1,14 @@
+# https://app.codesignal.com/challenge/mhQHptix9FRRyEmaz
+String SuperCup(String s) {
+  S = s;
+  return l("zamalek") < l("alahly") ? "win" : "loss";
+}
+
+String S;
+int l(String t) {
+  int i = 0;
+  for (int c : t.getBytes())
+    for (; S.charAt(i++) != c ;);
+    return i;
+}
+
