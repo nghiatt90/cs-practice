4d7041d17fd96d859ed6dc56929a3d74a040c2e7
Yoshi Truong <nghia.truong@tokyotechies.com>
2021-05-25 23:22:49 +0900

    shufflestate

---
+++ codesignal/shufflestate.py
@@ -0,0 +1,11 @@
+# https://app.codesignal.com/challenge/GDQ7MXLtpWKCMXNXy
+
+	int i, l, a[];
+
+	int[] shuffleState(int[] d, int n) {
+		a = d.clone();
+		l = a.length;
+		for (i = 0; i < l; )
+			a[i] = d[i / 2 + i++ % 2 * l / 2];
+		return n < 1 ? d : shuffleState(a, n - 1);
+	}
