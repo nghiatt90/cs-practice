4e995f29e9d6d9b4b626e09d6fc126af474b3e98
Yoshi Truong <nghia.truong@tokyotechies.com>
2021-05-25 23:18:20 +0900

    subarraysearch

---
+++ codesignal/subarraysearch.py
@@ -0,0 +1,7 @@
+# https://app.codesignal.com/challenge/ZMJNZNPzcNNS42eJR
+    int m, t;
+	Object subarraysearch(String S, String s) {
+	  for (int c : s.toLowerCase().getBytes())
+	    m = (t = S.toLowerCase().indexOf(c)) < m ? 99 : t;
+	  return m < 99;
+	}
