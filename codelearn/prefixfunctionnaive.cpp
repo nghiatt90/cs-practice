f07697c114ea52159d779a4cf4552fff71acd793
Yoshi Truong <nghia.truong@tokyotechies.com>
2021-05-25 16:25:12 +0900

    prefixfunctionnaive

---
+++ codelearn/prefixfunctionnaive.cpp
@@ -0,0 +1,12 @@
+// https://codelearn.io/training/detail/61092
+std::vector<int> prefixFunctionNaive(std::string s)
+{
+    int n = (int)s.length();
+    vector<int> pi(n);
+    for (int i = 0; i < n; i++)
+        for (int k = 0; k <= i; k++)
+            if (s.substr(0, k) == s.substr(i-k+1, k))
+                pi[i] = k;
+    return pi;
+}
+
