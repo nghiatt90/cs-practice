e5c635d66194b10f97a4f06956df42f639b5b580
Yoshi Truong <nghia.truong@tokyotechies.com>
2021-05-26 11:15:59 +0900

    palindrome_prime

---
+++ codesignal/palindrome_prime.py
@@ -0,0 +1,8 @@
+# https://app.codesignal.com/challenge/C6C8dC6bfGq5wu9ah
+int i, k;
+boolean palindrome_prime(int N) {
+  for (i = 1; ++i < N;)
+    k = N % i < 1 ? 1 : k;
+  return k < 1 & N > 1 & (N + "").equals(new StringBuffer(N + "").reverse().toString());
+}
+
