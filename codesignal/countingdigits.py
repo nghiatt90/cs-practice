6f4e1e445353e87acdc5cc71c0fe0f34979cb3a7
Yoshi Truong <nghia.truong@tokyotechies.com>
2021-05-26 11:10:37 +0900

    countingdigits

---
+++ codesignal/countingdigits.py
@@ -0,0 +1,8 @@
+# https://app.codesignal.com/challenge/gFiwQrkzufWjkscNA
+
+	int i, j = 1, r;
+	int CountingDigits(int N) {
+	  for (; N >= j; j *= 10)
+	    r += 9 * ++i * j;
+	  return r - (j - N - 1) * i;
+	}
