49aa322a112f235eede9e017b386679222c67c1b
Yoshi Truong <nghia.truong@tokyotechies.com>
2021-05-26 11:16:38 +0900

    piglatin

---
+++ codesignal/piglatin.py
@@ -0,0 +1,9 @@
+# https://app.codesignal.com/challenge/ihCrv87fdvkBhPMjq
+int i;
+String piglatin(String w) {
+  for (; w.matches("[^aiueoy].*"); ++i)
+    w = w.substring(1) + w.charAt(0);
+    
+  return w += i < 1 ? "way" : "ay";
+}
+
