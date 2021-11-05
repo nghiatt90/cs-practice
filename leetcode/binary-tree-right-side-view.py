dd170e810db572f35ccc083aa2eda642882ffb61
Yoshi Truong <nghiatt1030@gmail.com>
2021-11-05 15:52:49 +0900

    https://leetcode.com/problems/binary-tree-right-side-view/

---
+++ leetcode/binary-tree-right-side-view.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# cs-practice.binary-tree-right-side-view
+# Date: 2021/11/05
+# Filename: binary-tree-right-side-view
+
+__author__ = "Yoshi Truong"
+__date__ = "2021/11/05"
+
+from typing import Optional, List
+
+from leetcode.tree import TreeNode
+
+
+class Solution:
+    """https://leetcode.com/problems/binary-tree-right-side-view/"""
+
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+        queue = [root]
+        ans = []
+        while queue:
+            ans.append(queue[-1].val)
+            next_queue = []
+            for node in queue:
+                if node.left:
+                    next_queue.append(node.left)
+                if node.right:
+                    next_queue.append(node.right)
+            queue = next_queue
+        return ans
